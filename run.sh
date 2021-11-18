143970090059694a29338dcb5d0302c155ffc460
John Hollowell <johnahollowell@gmail.com>
2021-11-17 22:57:27 -0500

    Ensure pipe exists on each launch

--- run.sh
+++ run.sh
@@ -3,5 +3,11 @@
 # Start up cron
 sudo service cron start
 
+# Make sure the named pipe (FIFO) exists
+pipeFile=mc.pipe
+if [! -p "$pipeFile"]; then
+    mkfifo "$pipeFile"
+fi
+
 # Start up Minecraft
-tail -f mc.pipe | exec java -Xms$JAVA_MEMORY_MIN -Xmx$JAVA_MEMORY_MAX -jar -Dcom.mojang.eula.agree=true paper.jar
+tail -f "$pipeFile" | exec java -Xms$JAVA_MEMORY_MIN -Xmx$JAVA_MEMORY_MAX -jar -Dcom.mojang.eula.agree=true paper.jar
