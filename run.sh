7da2272ec94b26459cd60ca54ad342cf2c87d1a2
John Hollowell <johnahollowell@gmail.com>
2021-11-17 22:59:13 -0500

    Ensure env vars have value on run

--- run.sh
+++ run.sh
@@ -3,5 +3,9 @@
 # Start up cron
 sudo service cron start
 
+# Set defaults if environment variables are not set
+JAVA_MEMORY_MIN=${JAVA_MEMORY_MIN:-1G}
+JAVA_MEMORY_MAX=${JAVA_MEMORY_MAX:-4G}
+
 # Start up Minecraft
 tail -f mc.pipe | exec java -Xms$JAVA_MEMORY_MIN -Xmx$JAVA_MEMORY_MAX -jar -Dcom.mojang.eula.agree=true paper.jar
