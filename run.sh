c7150edfeb2a8bca2ec3cdbdcdf8b243250f5c3e
pyzaist <timothy.john.schwab@gmail.com>
2022-01-17 12:04:25 -0500

    comments

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Start up cron
 sudo service cron start
 
-# Make sure the named pipe (FIFO) exists
+# Create the named pipe and get it going
 pipeFile=mc.pipe
 if [ ! -p "$pipeFile" ]; then
     mkfifo "$pipeFile"
@@ -13,5 +13,5 @@ fi
 JAVA_MEMORY_MIN=${JAVA_MEMORY_MIN:-1G}
 JAVA_MEMORY_MAX=${JAVA_MEMORY_MAX:-4G}
 
-# Start up Minecraft
+# Start up Minecraft, listening to the named pipe
 tail -f "$pipeFile" | exec java -Xms$JAVA_MEMORY_MIN -Xmx$JAVA_MEMORY_MAX -jar -Dcom.mojang.eula.agree=true paper.jar
